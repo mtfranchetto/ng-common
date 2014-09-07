617a2d0803d8f34269987db5d6d42296f5129194
Mattia Franchetto <mt.franchetto@gmail.com>
2014-09-07 18:07:11 +0200

    Add index exports

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-'use strict';
+"use strict";
 
-require('angular');
-require('jquery');
-require('underscore');
+module.exports = {
+    inherits: require('inherits'),
+    BaseExecutor: require('./lib/BaseExecutor'),
+    ValueHolder: require('./lib/ValueHolder'),
+    ConsoleLogger: require('./lib/ConsoleLogger'),
+    Logger: require('./lib/Logger'),
+    io: {
+        Gson: require('./lib/io/Gson'),
+        JSONSerializer: require('./lib/io/JSONSerializer'),
+        Serializer: require('./lib/io/Serializer'),
+        SettingsManager: require('./lib/io/SettingsManager')
+    },
+    net: {
+        Cache: require('./lib/net/Cache'),
+        MemCache: require('./lib/net/MemCache'),
+        HttpClient: require('./lib/net/HttpClient'),
+        GsonParser: require('./lib/net/GsonParser'),
+        Parser: require('./lib/net/Parser'),
+        RequestHandler: require('./lib/net/RequestHandler')
+    },
+    angular: {
+        ScopeInvoker: require('./lib/angular/ScopeInvoker')
+    }
 
-//Custom rules
+};
