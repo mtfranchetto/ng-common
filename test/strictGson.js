b9038fbe6688ed1e74e93cb9053c618bc2aab780
Mattia Franchetto <mt.franchetto@gmail.com>
2015-02-02 12:51:49 +0100

    Add test with strict gson nested data

--- test/strictGson.js
+++ test/strictGson.js
@@ -6,6 +6,7 @@ describe('Strict gson', function () {
 
     var Gson = require('../lib/io/StrictGson'),
         TestModel = require('./data/TestModel'),
+        LittleTestModel = require('./data/LittleTestModel'),
         Country = require('./data/Country');
 
 
@@ -24,6 +25,29 @@ describe('Strict gson', function () {
         expect(obj.password).toEqual("doe");
         expect(obj.getFoo()).toEqual("foo");
         expect(obj instanceof TestModel).toBeTruthy();
+    });
+
+    it('should return an object with a TestModel array', function () {
+        var gson = new Gson(),
+            json = {
+                "user": "john",
+                "password": "doe",
+                "others": [
+                    {
+                        "user": "foo",
+                        "password": "bar"
+                    }
+                ],
+                "testModel": {}
+            };
+
+        var obj = gson.deserialize(json, [TestModel, LittleTestModel]);
+        expect(obj.user).toEqual("john");
+        expect(obj.password).toEqual("doe");
+        expect(obj.others[0].user).toEqual("foo");
+        expect(obj.others[0].password).toEqual("bar");
+        expect(obj instanceof TestModel).toBeTruthy();
+        expect(obj.others[0] instanceof LittleTestModel).toBeTruthy();
     });
 
 });
